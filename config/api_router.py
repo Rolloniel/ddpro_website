f07ffcbd38d16086d76c1b4ac7f5acc15952f5c4
DanKliuiev <dankliuiev@planeks.net>
2020-03-06 12:17:48 +0200

    changed api structure

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from ddpro_website.users.api.views import UserViewSet
+from ddpro_website.users.api.v1.views import UserViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
