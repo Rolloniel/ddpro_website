ce143fcd28b791525c496425289c9eef6a276ff6
DanKliuiev <dankliuiev@planeks.net>
2020-03-09 22:43:56 +0200

    added about url to router

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from ddpro_website.users.api.v1.views import UserViewSet
+from ddpro_website.base_ui.views import AboutViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -9,6 +10,7 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("about", AboutViewSet)
 
 
 app_name = "api"
