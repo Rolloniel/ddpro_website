8b3f5b80086578747864f7264a2a751368cb4687
DanKliuiev <dankliuiev@planeks.net>
2020-03-09 22:37:39 +0200

    added admin models for About, TeamMember, Product models

--- ddpro_website/base_ui/admin.py
+++ ddpro_website/base_ui/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import About, TeamMember, Product
 
-# Register your models here.
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+   list_display = ['heading', 'text_html']
+
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+   list_display = ['first_name', 'last_name', 'image', 'priority', 'roles']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+   list_display = ['title', 'image', 'short_description']
